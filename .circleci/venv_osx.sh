a3822d9a451cc100a23d3e364865364119c8cac5
epiphyte <epiphyte@vertex.link>
2018-04-28 14:44:58 -0400

    Tweak venv_osx

--- .circleci/venv_osx.sh
+++ .circleci/venv_osx.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 source ~/.bash_profile
+ls -ltha ~/
 if [ -d ~/.pyenv ]; then
   pyenv versions | grep syn36
   if [ $? -eq 0 ]; then
+    echo "syn36 venv found"
     exit 0
   fi
 else
+  echo "No .pyenv found"
   exit 1
 fi
 
-# Make a venv from 3.6.5 and install some base packages in it
+echo "Adding syn36 venv and installing packages"
 pyenv virtualenv --copies 3.6.5 syn36
 pyenv shell syn36
 python3 -m pip install -U wheel pip pytest pytest-cov pycodestyle codecov
